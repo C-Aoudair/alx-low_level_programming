7fc64142edf2d9d6f329feab07a9c7598d6bf803
AOUDAI <aoudair16031999@gmail.com>
2023-08-10 20:59:13 +0000

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main - An entry point
+ *
+ * Return: When 1 (Success)
+ */
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
