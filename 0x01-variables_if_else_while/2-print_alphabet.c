6959f2d796a294f9c2f4fa4e5912686074bbb8c7
AOUDAI <aoudair16031999@gmail.com>
2023-08-11 15:30:32 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - An entry point
+ *
+ * Return: When 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
