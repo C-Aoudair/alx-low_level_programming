6b25a74a645de81911ab4106f61093036dd80d5c
AOUDAI <aoudair16031999@gmail.com>
2023-08-11 16:17:24 -0700

    numbers

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - An entry point
+ *
+ * Return: When 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+			putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
