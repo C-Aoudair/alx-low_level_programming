4ad2f9bf202d2e6bc165a6b966642633fd56e4a5
AOUDAI <aoudair16031999@gmail.com>
2023-08-12 02:39:08 -0700

    test

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ * main - An entry point
+ *
+ * Return: When 0 (Success)
+ */
+int main(void)
+{
+int i;
+int m = 0;
+int j;
+
+for (i = '0'; i <= '9'; i++)
+{
+	for (j = m; j <= '9'; j++)
+	{
+	putchar(i);
+	putchar(j);
+	if (i != '8' && j != '9')
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	m++;
+}
+putchar('\n');
+return (0);
+}
