d67be47df8d22b399a5a9b9d435ff2f679d7fa78
AOUDAI <aoudair16031999@gmail.com>
2023-08-12 04:28:12 -0700

    comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,7 +7,9 @@
 int main(void)
 {
 int i;
-int m = ' 1';
+int m = '1';
+int n = '2';
+int k;
 int j;
 
 for (i = '0'; i <= '9'; i++)
@@ -15,15 +17,22 @@ for (i = '0'; i <= '9'; i++)
 	j = m;
 	for ( ; j <= '9'; j++)
 	{
-		putchar(i);
-		putchar(j);
-		if (i != '8' || j != '9')
+		k = n;
+		for ( ; k <= '9'; k++)
 		{
-			putchar(',');
-			putchar(' ');
+			putchar(i);
+			putchar(j);
+			putchar(k);
+			if (i != '7' || j != '8' || k != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
+	n++;
 	}
 	m++;
+	n = m + 1;
 }
 putchar('\n');
 return (0);
