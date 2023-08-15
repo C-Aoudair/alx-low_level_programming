0f9e054380e0fc3acac3f43c879bad2dcce10c81
AOUDAI <aoudair16031999@gmail.com>
2023-08-15 10:56:49 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit of a number
+ * @n: the number
+ *
+ * Ruturn: the last digit
+ */
+int print_last_digit(int n)
+	return  (n % 10);
