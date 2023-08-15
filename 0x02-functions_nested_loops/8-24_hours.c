694584c66ab3f8f2de128316882086b89602e489
AOUDAI <aoudair16031999@gmail.com>
2023-08-15 15:09:01 -0700

    jack

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,15 +10,15 @@ void jack_bauer(void)
 	int k;
 	int l;
 
-	for (i == 0; i <= 2; i++)
+	for (i = 0; i <= 2; i++)
 	{
-		for (j == 0; j <= 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			if (i == 2 && j == 4)
 				break;
-			for (k == 0; k <= 5; k++)
+			for (k = 0; k <= 5; k++)
 			{
-				for (l == 0; l <= 9; l++)
+				for (l = 0; l <= 9; l++)
 				{
 					_putchar('0' + i);
 					_putchar('0' + j);
