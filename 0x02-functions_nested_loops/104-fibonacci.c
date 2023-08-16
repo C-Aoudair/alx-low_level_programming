9b893383327f843f5c43a8f4b6e297d707809136
AOUDAI <aoudair16031999@gmail.com>
2023-08-16 15:48:55 -0700

    last one

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main - print the numbers.
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	int i;
+	unsigned long m = 1;
+	unsigned long n = 2;
+	unsigned long k;
+
+	printf("%lu, %lu", m, n);
+	for (i = 0; i < 95; i++)
+	{
+		k = m + n;
+		m = n;
+		n = k;
+		printf(", %lu", k);
+	}
+	printf("\n");
+	return (0);
+}
