944cb3e930c64558f414a709c652d7a629aed91e
AOUDAI <aoudair16031999@gmail.com>
2023-08-16 11:52:25 -0700

    times tible

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * print_times_table - print the n table starting with 0
+ *
+ */
+void print_times_table(int n)
+{
+	int m;
+	int l;
+if (n > 15 || n < 0)
+{
+	for (int i = 0; i <= n; i++)
+	{
+		_putchar('0');
+		for (int j = 1; j <= n; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			l = j * i;
+			if (l >= 100)
+			{
+				m = l % 100;
+				_putchar('0' + l / 100);
+				_putchar('0' + m / 10);
+				_putchar('0' + m);
+			}
+			else if (l >= 10)
+			{
+				_putchar(' ');
+				_putchar('0' + l / 10);
+				_putchar('0' + l % 10);
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + l);
+			}
+			_putchar('\n');
+		}
+	}
+}
+}
