6d76255789557c5400b20e895f63bbfebf77afb6
AOUDAI <aoudair16031999@gmail.com>
2023-08-21 04:13:27 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-srelen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - get the length of the string.
+ * @s: the string.
+ *
+ * Return: the length of the string.
+ */
+int _strlen(char *s)
+{
+	int n = 0;
+
+	do
+		n++;
+	while
+		s[n] != '\0';
+}
+
