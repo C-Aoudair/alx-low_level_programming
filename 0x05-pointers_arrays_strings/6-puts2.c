5a748e79f24cc7659404c7ee47c0affd586e62d7
AOUDAI <aoudair16031999@gmail.com>
2023-08-21 05:40:18 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - print the characters of a string.
+ * @str: the string.
+ *
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+		-putchar('\n');
+	}
+}
+
