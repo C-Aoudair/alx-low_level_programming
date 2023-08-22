2e6bd52bc18128f56b9f85791d58139b3ec48961
AOUDAI <aoudair16031999@gmail.com>
2023-08-22 10:45:08 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,35 +8,10 @@
  */
 int _atoi(char *s)
 {
-        int i, j, k, n, m, l;
+        int i = 0, j, k, n, m, l;
         int res = 0;
-
-        for (i = 0; s[i] < '9'; i++)
-        {
-                if ((s[i] = '\0'))
-                {
-                        return 1;
-                        j = i + 1;
-                        do
-                        {
-                                i++;
-  }
-                        while (s[i] >= '0' || s[i] <= '9');
-                        if (j != i)
-                        {
-                                for (k = j; k < i; k++)
-                                {
-                                        n = (i - j - 1);
-                                        for (l = 0; l <= 9; l++)
-                                        {
-                                                if ((s[k] = '0' + l))
-                                                        m = l;
-                                        }
-                                        res = res + m * (10 ^ n);
-                                        n--;
-                                }
-                        }
-                }
-        }
+       	while (s[i] < '0' || s[i] > 9)
+	{
+		return 0;
         return (res);
 }
