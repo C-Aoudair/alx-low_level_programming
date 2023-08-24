7643da3398beb4900bbcb839c7331d8fce87bbf5
AOUDAI <aoudair16031999@gmail.com>
2023-08-24 03:16:42 -0700

    copy

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 /**
  * _strncat - concatenate two string.
  * @dest: first string.
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncpy - copy the string.
+ * @src: the string that must be copied:
+ * @dest: the copied string.
+ * @n: the number of bytes.
+ *
+ * Return: the copied string.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
