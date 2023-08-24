e02f26347997a46fb60d410aebc7d252b307e4f1
AOUDAI <aoudair16031999@gmail.com>
2023-08-24 14:25:55 -0700

    cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,50 +8,26 @@
  */
 char *cap_string(char *s)
 {
-	int i;
+int i;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == ' ' || s[i] == '	' || s[i] == ',')
-		{
-			i++;
-			if (s[i] >= 'a' && s[i] <= 'z')
-				s[i] = s[i] - 32;
-			else
-				i--;
-		}
-		else if (s[i] == '\n' || s[i] == ';' || s[i] == '.')
-		{
-			i++;
-			if (s[i] >= 'a' && s[i] <= 'z')
-				s[i] = s[i] - 32;
-			else
-				i--;
-		}
-		else if (s[i] == '!' || s[i] == '?' || s[i] == '"')
-		{
-			i++;
-			if (s[i] >= 'a' && s[i] <= 'z')
-				s[i] = s[i] - 32;
-			else
-				i--;
-		}
-		else if ( s[i] == '(' || s[i] == ')' || s[i] == '{')
-		{
-			i++;
-			if (s[i] >= 'a' && s[i] <= 'z')
-				s[i] = s[i] - 32;
-			else
-				i--;
-		}
-		else if (s[i] == '}')
-		{
-			i++;
-			if (s[i] >= 'a' && s[i] <= 'z')
-				s[i] = s[i] - 32;
-			else
-				i--;
-		}
-	}
-	return (s);
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == ' ' || s[i] == '	' || s[i] == ',' || s[i] == '(' || s[i] == ')' || s[i] == '{')
+{
+i++;
+if (s[i] >= 'a' && s[i] <= 'z' && s[i] != '\0')
+s[i] = s[i] - 32;
+else
+i--;
+}
+else if (s[i] == '\n' || s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?' || s[i] == '"' s[i] == '}')
+{
+i++;
+if (s[i] >= 'a' && s[i] <= 'z' && s[i] != '\0')
+s[i] = s[i] - 32;
+else
+i--;
+}
+}
+return (s);
 }
