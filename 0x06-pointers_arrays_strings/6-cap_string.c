f0c646e07e47e2b9e09b1b212154d2c163090432
AOUDAI <aoudair16031999@gmail.com>
2023-08-24 12:00:20 -0700

    cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * cap_string - capitalizes all words of a string.
  * @s: the string.
@@ -11,12 +12,56 @@ char *cap_string(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == ' ' || s[i] == '	' || s[i] == ',' || s[i] == '\n' || s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?' || s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
+		if (s[i] == ' ' || s[i] == '	' || s[i] == ',')
 		{
 			i++;
 			if (s[i] >= 'a' && s[i] <= 'z')
 				s[i] = s[i] - 32;
+			else
+				i--;
+		}
+		else if (s[i] == '\n' || s[i] == ';' || s[i] == '.')
+		{
+			i++;
+			if (s[i] >= 'a' && s[i] <= 'z')
+				s[i] = s[i] - 32;
+			else
+				i--;
+		}
+		else if (s[i] == '!' || s[i] == '?' || s[i] == '"')
+		{
+			i++;
+			if (s[i] >= 'a' && s[i] <= 'z')
+				s[i] = s[i] - 32;
+			else
+				i--;
+		}
+		else if ( s[i] == '(' || s[i] == ')' || s[i] == '{')
+		{
+			i++;
+			if (s[i] >= 'a' && s[i] <= 'z')
+				s[i] = s[i] - 32;
+			else
+				i--;
+		}
+		else if (s[i] == '}')
+		{
+			i++;
+			if (s[i] >= 'a' && s[i] <= 'z')
+				s[i] = s[i] - 32;
+			else
+				i--;
 		}
 	}
 	return (s);
+}
+int main(void)
+{
+    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+    char *ptr;
+
+    ptr = cap_string(str);
+    printf("%s", ptr);
+    printf("%s", str);
+    return (0);
 }
