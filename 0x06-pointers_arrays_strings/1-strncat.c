c425d9255191e5421df609ebeae3fba7b8858814
AOUDAI <aoudair16031999@gmail.com>
2023-08-24 02:40:52 -0700

    strncaat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * _strncat - concatenate two string.
  * @dest: first string.
@@ -15,7 +16,7 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		i++;
 	}
-	j = i + 1;
+	j = strlen(dest);
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[j + i] = src[i];
