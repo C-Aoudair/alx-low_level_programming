33d4e9044b5d4634e00802672d0301a057326128
AOUDAI <aoudair16031999@gmail.com>
2023-08-24 03:47:14 -0700

    copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _strncpy - copy the string.
  * @src: the string that must be copied:
@@ -13,7 +14,11 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		(dest + i) =( src + i);
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
 	}
 	return (dest);
 }
