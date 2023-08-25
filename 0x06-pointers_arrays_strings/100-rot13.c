42654e47a4d894ac35ec67201b37651a765eb1dc
AOUDAI <aoudair16031999@gmail.com>
2023-08-25 05:03:34 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13.
+ * @s: the string.
+ *
+ * Return: the string encoded.
+ */
+char *rot13(char *s)
+{
+	int i;
+
+	for (i = 1; s[i] != '\0' && s[i - 1] != '\0'; i++)
+	{
+	while (s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z')
+	{
+	if (s[i] >= 'a' && s[i] <= 'm' || s[i] >= 'A' && s[i] <= 'M')
+		s[i] = s[i] + 13;
+	else
+		s[i] = s[i] - 13;
+	i++;
+	}
+	}
+}
