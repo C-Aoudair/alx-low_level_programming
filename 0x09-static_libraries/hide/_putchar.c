91eab778840dd9f91b288576693c85ed3ff94e99
AOUDAI <aoudair16031999@gmail.com>
2023-09-02 00:09:53 +0100

    static libraries

--- 0x09-static_libraries/hide/_putchar.c
+++ 0x09-static_libraries/hide/_putchar.c
@@ -1,1 +1,12 @@
-
+#include <stdio.h>
+/**
+ * _putchar - print a character to the screen.
+ * @c: the character.
+ *
+ * Return: nothing.
+ */
+int _putchar(char c)
+{
+	putchar(c);
+	return (0);
+}
