2f0fff1bedae188c29fe4cd03eb39c1cc9f144ba
AOUDAI <aoudair16031999@gmail.com>
2023-09-02 01:44:10 +0100

    argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints its name.
+ * @argc: the number of the cl arguments.
+ * @argv: cl arguments.
+ *
+ * Return: always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
