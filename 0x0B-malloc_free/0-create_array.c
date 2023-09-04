d216b63bdb50b209ce8857c2c1ddbba92b45a93a
AOUDAI <aoudair16031999@gmail.com>
2023-09-04 15:35:32 +0100

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,11 +13,15 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *array;
 
-
+	if (size == 0)
+		return (NULL);
 	array = malloc(size);
-	for (i = 0; i < size; i++)
+	if (array != NULL)
 	{
-		array[i] = c;
+		for (i = 0; i < size; i++)
+		{
+			array[i] = c;
+		}
 	}
 	return (array);
 }
