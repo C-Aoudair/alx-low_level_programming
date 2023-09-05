511776c9916bd8ebfabde4966afe9cc44496ce33
AOUDAI <aoudair16031999@gmail.com>
2023-09-05 15:19:34 +0100

    concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,53 +11,41 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int i;
-	unsigned int n1, n2;
+	unsigned int i, n1, n2;
 	char *con;
 
 	if (s1 == NULL && s2 != NULL)
 	{
 		con = malloc(strlen(s2) + 1);
-		for (i = 0; i <= strlen(s2); i++)
-		{
+		if (con == NULL)
+			return (NULL);
+		for (i = 0, n2 = strlen(s2); i <= n2; i++)
 			con[i] = s2[i];
-		}
-		return (con);
 	}
-	if (s1 != NULL && s2 == NULL)
+	else if (s1 != NULL && s2 == NULL)
 	{
 		con = malloc(strlen(s1) + 1);
-		for (i = 0; i <= strlen(s1); i++)
-		{
+		if (con == NULL)
+			return (NULL);
+		for (i = 0, n1 = strlen(s1); i <= n1; i++)
 			con[i] = s1[i];
-		}
-		return (con);
 	}
-	if (s1 == NULL && s2 == NULL)
+	else if (s1 == NULL && s2 == NULL)
 	{
 		con = malloc(1);
-		if (con != NULL)
-			con[0] = '\0';
-		return (con);
+		if (con == NULL)
+		return (NULL);
+		con[0] = '\0';
 	}
-	n1 = strlen(s1);
-	n2 = strlen(s2);
-	con = malloc(n1 + n2 + 1);
-	if (con != NULL)
+	else
 	{
-		if (n1 == 0 && n2 == 0)
-			con[0] ='\0';
-		else
-		{
-			for (i = 0; i < n1; i++)
-			{
-				con[i] = s1[i];
-			}
-			for (i = 0; i <= n2; i++)
-			{
-				con[i + n1] = s2[i];
-			}
-		}
+		con = malloc(strlen(s1) + strlen(s2) + 1);
+		if (con == NULL)
+			return (NULL);
+		for (i = 0, n1 = strlen(s1); i < n1; i++)
+			con[i] = s1[i];
+		for (i = 0, n2 = strlen(s2); i <= n2; i++)
+			con[i + n1] = s2[i];
 	}
 	return (con);
 }
