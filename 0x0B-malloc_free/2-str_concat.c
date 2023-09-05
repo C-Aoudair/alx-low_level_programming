6b2e010a967297ebe57b50fa183043626118dcd5
AOUDAI <aoudair16031999@gmail.com>
2023-09-05 15:52:23 +0100

    concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 /**
  * str_concat - concatenates two strings.
  * @s1: The first string.
@@ -14,38 +13,30 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, n1, n2;
 	char *con;
 
-	if (s1 == NULL && s2 != NULL)
+	if (s1 == NULL && s2 == NULL)
+		n1 = n2 = 0;
+	else if (s1 == NULL && s2 != NULL)
 	{
-		con = malloc(strlen(s2) + 1);
-		if (con == NULL)
-			return (NULL);
-		for (i = 0, n2 = strlen(s2); i <= n2; i++)
-			con[i] = s2[i];
+		n1 = 0;
+		n2 = strlen(s2);
 	}
 	else if (s1 != NULL && s2 == NULL)
 	{
-		con = malloc(strlen(s1) + 1);
-		if (con == NULL)
-			return (NULL);
-		for (i = 0, n1 = strlen(s1); i <= n1; i++)
-			con[i] = s1[i];
-	}
-	else if (s1 == NULL && s2 == NULL)
-	{
-		con = malloc(1);
-		if (con == NULL)
-		return (NULL);
-		con[0] = '\0';
+		n1 = strlen(s1);
+		n2 = 0;
 	}
 	else
 	{
-		con = malloc(strlen(s1) + strlen(s2) + 1);
-		if (con == NULL)
-			return (NULL);
-		for (i = 0, n1 = strlen(s1); i < n1; i++)
-			con[i] = s1[i];
-		for (i = 0, n2 = strlen(s2); i <= n2; i++)
-			con[i + n1] = s2[i];
+		n1 = strlen(s1);
+		n2 = strlen(s2);
 	}
+	con = malloc(n1 + n2 + 1);
+	if (con == NULL)
+		return (NULL);
+	for (i = 0; i < n1; i++)
+		con[i] = s1[i];
+	for (i = 0; i < n2; i++)
+		con[i + n1] = s2[i];
+	con[i + n1] = '\0';
 	return (con);
 }
