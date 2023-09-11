7503a5070b5207c85b750d551920cca7f98129f3
AOUDAI <aoudair16031999@gmail.com>
2023-09-11 22:58:38 +0100

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,8 +13,9 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
+
 	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
+	if (dog == NULL || name == NULL || owner == NULL)
 		return (NULL);
 	dog->name = name;
 	dog->age = age;
