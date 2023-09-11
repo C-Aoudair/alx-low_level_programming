ec7fee135e6fc80d50cf663c1ee9ddff6e523fee
AOUDAI <aoudair16031999@gmail.com>
2023-09-11 18:46:16 +0100

    initialize struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - initialize a variable of type truct dog.
+ * @d: The name of the struct.
+ * @name: The first parameter of the struct.
+ * @age: The second parameter of the struct.
+ * @owner: The third parameter of the struct.
+ *
+ * Return: nothing.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
