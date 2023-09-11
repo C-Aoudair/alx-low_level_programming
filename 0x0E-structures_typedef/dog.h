658075a65e5554c1ea06e1481969eda959b84cfd
AOUDAI <aoudair16031999@gmail.com>
2023-09-11 18:27:14 +0100

    First struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - discription for a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * description for the properties of a gevin dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
