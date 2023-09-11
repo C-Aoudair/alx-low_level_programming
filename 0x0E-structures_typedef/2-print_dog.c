a7c36f001f5ffb7c0ffc843c5a4fd19e727d2f28
AOUDAI <aoudair16031999@gmail.com>
2023-09-11 23:14:39 +0100

    old dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,11 +10,11 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("Name: %s\n", (d->name == NULL) ? "nil" : d->name);
+		printf("Name: %s\n", (d->name == NULL) ? "(nil)" : d->name);
 		if (d->age)
 			printf("Age: %f\n", d->age);
 		else
 			printf("Age: \n");
-		printf("Owner: %s\n", (d->owner == NULL) ? "nil" : d->owner);
+		printf("Owner: %s\n", (d->owner == NULL) ? "(nil)" : d->owner);
 	}
 }
