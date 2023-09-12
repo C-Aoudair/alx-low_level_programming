a62f756fd8645c9a76c0a8cc40b6d657d2c86af7
AOUDAI <aoudair16031999@gmail.com>
2023-09-12 20:35:04 +0100

    fix the error

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,12 +8,14 @@
  */
 void print_dog(struct dog *d)
 {
+	int x = 0;
+
 	if (d != NULL)
 	{
 		printf("Name: %s\n", (d->name == NULL) ? "(nil)" : d->name);
 		printf("Owner: %s\n", (d->owner == NULL) ? "(nil)" : d->owner);
 		if (d->name == NULL && d->owner == NULL)
-			d->age = (float)0;
+			d->age = (float)x;
 		printf("Age: %f\n", d->age);
 	}
 }
