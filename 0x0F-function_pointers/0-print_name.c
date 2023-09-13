7dc3ff6c0c160088955d65a9962a18fc2afe3d28
AOUDAI <aoudair16031999@gmail.com>
2023-09-13 16:38:42 +0100

    first task

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - prints a name.
+ * @name: The name.
+ * @f: A pointer to a funtion that prints the name.
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
