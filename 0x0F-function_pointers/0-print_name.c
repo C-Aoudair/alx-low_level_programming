a46cef93e48acc4d1872aca1aa34054c21a1e2aa
AOUDAI <aoudair16031999@gmail.com>
2023-09-14 13:33:04 +0100

    fix task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include <stdio.h>
 /**
  * print_name - prints a name.
  * @name: The name.
@@ -7,6 +8,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL))
+	if (name != NULL && f != NULL)
 		f(name);
 }
