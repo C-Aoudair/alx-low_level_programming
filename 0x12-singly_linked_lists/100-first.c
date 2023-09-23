e8c81b6f1e64292696c021109038e0e14bcec360
AOUDAI <aoudair16031999@gmail.com>
2023-09-23 11:45:51 +0100

    Add the advanced task 1

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * first - Executed before the main function.
+ *
+ */
+void first(void) __attribute__ ((constructor));
+
+void first(void)
+{
+	char *str = "I bore my house upon my back!";
+	
+	printf("You're beat! and yet, you must allow,\n%s\n", str);
+}
