bc454578bf58428260f6f6f2a00b2666cfcda34c
AOUDAI <aoudair16031999@gmail.com>
2023-09-23 11:47:09 +0100

    Add the advanced task 1

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,6 +10,6 @@ void first(void) __attribute__ ((constructor));
 void first(void)
 {
 	char *str = "I bore my house upon my back!";
-	
+
 	printf("You're beat! and yet, you must allow,\n%s\n", str);
 }
