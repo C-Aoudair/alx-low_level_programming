0f14accce796cdd95f4d27971e431770b2377e1b
AOUDAI <aoudair16031999@gmail.com>
2023-09-25 18:06:31 +0100

    Adds task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+
+/**
+ * free_listint2 - Frees a listint_t list.
+ * @head: The list.
+ *
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	while (*head != NULL)
+	{
+		ptr = (*head)->next;
+		free(*head);
+		*head = ptr;
+	}
+	*head = NULL;
+
+}
+
