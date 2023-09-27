33ebeb01aa63f25b2c9e754e3997c3a3051ac6e5
AOUDAI <aoudair16031999@gmail.com>
2023-09-27 15:29:05 +0100

    Add 101-print_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+
+/**
+ * print_listint_safe - Prints a listint_t linked list. with a loop.
+ * @head: The list.
+ *
+ * Return: The number of node int the list.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	listint_t const **list;
+	const listint_t *ptr;
+	size_t count, i;
+
+	list = malloc(8);
+	*list = head;
+	count = 0;
+	for (ptr = head; ptr->next != NULL; ptr = ptr->next)
+	{
+		printf("[%p] %d\n", (void *)ptr, ptr->n);
+		count = count + 1;
+		for (i = 0; i < count; i++)
+			if (ptr->next == *(list + i))
+			{
+				ptr = ptr->next;
+				printf("-> [%p] %d\n", (void *)ptr, ptr->n);
+				free(list);
+				return (count);
+			}
+		list = realloc(list, (1 + count) * 8);
+		*(list + count) = ptr->next;
+	}
+	free(list);
+	return (count);
+}
+
