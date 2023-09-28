14cb370353efb3976d30ea9a9eaf37d2e9c82c26
AOUDAI <aoudair16031999@gmail.com>
2023-09-28 19:58:40 +0100

    Add task 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,19 +2,19 @@
 
 
 /**
- * set_bit - sets the value of a bit to 1 at a given index.
+ * clear_bit - sets the value of a bit to 1 at a given index.
  * @n: The address of the number.
  * @index: The index.
  *
  * Return: 1 if worked or -1 if an error occured.
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int bit_mask;
 
 	if (index > 63)
 		return (-1);
 	bit_mask = 1;
-	*n = (*n & bit_mask << index);
+	*n = (*n & ~(bit_mask << index));
 	return (1);
 }
