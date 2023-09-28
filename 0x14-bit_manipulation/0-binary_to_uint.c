a11d545b29ee3042e0e9fd87d5bf2378221a183a
AOUDAI <aoudair16031999@gmail.com>
2023-09-28 13:16:05 +0100

    Fix task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,8 +1,23 @@
 #include "main.h"
 #include <string.h>
-#include <math.h>
 
 
+/**
+ * Pow - calculate the power of a number.
+ * @x: The base.
+ * @y: The exponent.
+ *
+ * Return: The result.
+ */
+int Pow(int x, int y)
+{
+	int result, i;
+
+	result = 1;
+	for (i = 0; i < y; i++)
+		result = result * x;
+	return (result);
+}
 /**
  * binary_to_uint - converts a binary number ot an unsigned int.
  * @b: A pointer to a string of 0 and 1 chars.
@@ -18,7 +33,7 @@ unsigned int binary_to_uint(const char *b)
 	for (i = 0, j = strlen(b); i < j; i++)
 	{
 		if (b[j - i - 1] == '1')
-			result = result + pow(2, i);
+			result = result + Pow(2, i);
 		else if (b[j - i - 1] == '0')
 			continue;
 		else
