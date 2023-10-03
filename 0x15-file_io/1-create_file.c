486468221dfde924f182bb45a436182da215ba62
AOUDAI <aoudair16031999@gmail.com>
2023-10-03 13:37:02 +0100

    Fix task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,9 +12,13 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 
+	if (filename == NULL)
+		return (-1);
 	fd = creat(filename, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
-	write(fd, text_content, strlen(text_content));
+	if (text_content != NULL)
+		write(fd, text_content, strlen(text_content));
+	close(fd);
 	return (1);
 }
