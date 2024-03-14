0e9dd8751244d180d601994bdf3951b1d51fa4d3
AOUDAI <aoudair16031999@gmail.com>
2024-03-15 00:13:55 +0100

    add task 2

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,50 @@
+#include "search_algos.h"
+
+
+/**
+ * binary_assistance - assist the binary_search function
+ * @start: location to start search from
+ * @end: location to end searching at
+ * @array: A pointer to the first element of the array to search
+ * @value: The value to search for
+*/
+int binary_assistance(int *array, size_t start, size_t end, int value)
+{
+    size_t middle, i;
+
+    if (start > end)
+        return (-1);
+    
+    printf("Serching in array:");
+    for (i = start; i < end; i++)
+        printf("%i, ", array[i]);
+    printf("%i\n", array[i]);
+    
+    middle = (end + start) / 2;
+    if (array[middle] == value)
+        return (middle);
+    if (array[middle] < value)
+        start = start + middle + 1;
+    else
+        end = middle - 1;
+    
+    return (binary_assistance(array, start, end, value));
+}
+
+
+/**
+ * binary_search - searchs for a value in an array of integers
+ * @size: the number of element in array
+ * @array: A pointer to the first element of the array to search
+ * @value: The value to search for
+ * 
+ * Return: The index of the value of -1 if not found or array is null
+*/
+
+int binary_search(int *array, size_t size, int value)
+{
+    if (array == NULL)
+        return (-1);
+
+    return (binary_assistance(array, 0, size - 1, value));
+}
