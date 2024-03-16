a0fd011aef48ed4bcd31463977da26f1a761c96f
AOUDAI <aoudair16031999@gmail.com>
2024-03-16 18:05:26 +0100

    test

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,7 +8,6 @@ int advanced_binary(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
-listint_t *jump_list(listint_t *list, size_t size, int value);
 
 /*=============================================================*/
 /**
@@ -26,5 +25,6 @@ typedef struct listint_s
     size_t index;
     struct listint_s *next;
 } listint_t;
+listint_t *jump_list(listint_t *list, size_t size, int value);
 
 #endif
